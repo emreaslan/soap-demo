709ee2d1a84a3f91ae70628bff616eac881c8ff6
Emre Sercan Aslan <emre55aslan@gmail.com>
2019-11-10 23:46:47 +0300

    first commit

---
+++ soap-service/src/test/java/com/example/soapservice/SoapServiceApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.soapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
